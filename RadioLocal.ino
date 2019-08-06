9946d9bd9f797b9ed1295d3c0b88b06c4804397b
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-08-06 02:13:35 -0700

    SD card fallback

--- RadioLocal.ino
+++ RadioLocal.ino
@@ -7,17 +7,37 @@
 #include <SPIFFS.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <SD.h>
 
 // Default STA settings
-const char* ssid = "SBG6700AC-77379";
-const char* pswd = "2fa66a5417";
-
+const char* ssid = "Turbocharged";
+const char* pswd = "wholefoods";
+File dataFile;
 // Web server settings
 AsyncWebServer server(80);
 const char* host = "radio";
 
+String getMIME(String filename) {
+  if (filename.endsWith(".png")) {
+    return "image/png";
+  } else if (filename.endsWith(".jpg")) {
+    return "image/jpeg";
+  } else if (filename.endsWith(".mp3")) {
+    return "audio/mp3";
+  }
+  return "text/plain";
+}
+
 // On not found - checks SD card if file exists
 void handleError(AsyncWebServerRequest *request) {
+  dataFile = SD.open(request->url());
+  if (dataFile) {
+    request->send(getMIME(request->url()), dataFile.size(), 
+      [](uint8_t *buffer, size_t maxLen, size_t index) -> size_t {
+      return dataFile.read(buffer, maxLen); });
+    return;
+  } 
+  
   request->send(404, "text/plain", "Page not found.");
 }
 
@@ -27,20 +47,26 @@ void setup() {
   // Connect WiFi
   WiFi.mode(WIFI_STA);
   if (!WiFi.begin(ssid, pswd)) {
-    Serial.println("Error connecting to WiFi.");
+    Serial.println("Error connecting to WiFi!");
     return;
   }
 
   // Begin MDNS
   if (!MDNS.begin(host)) {
-    Serial.println("Error setting up MDNS responder!");
+    Serial.println("Error setting up MDNS responder!!");
     return;
   }
   
   // Initialize SPIFFS
   if(!SPIFFS.begin(true)){
-    Serial.println("An error has occurred while mounting SPIFFS");
+    Serial.println("An error has occurred while mounting SPIFFS!!!");
     return;
+  }
+
+  // Initialize SD card
+  if(!SD.begin(SS)) {
+    Serial.println("An error has occured mounting the SD card!!!!");
+    Serial.println("The server will operate in a limited capacity.");
   }
   
   // On 404 error
@@ -84,7 +110,7 @@ void setup() {
     request->send(SPIFFS, "/media/rlinvertsmall.jpg", "image/jpeg");
   });
 
-  // Music
+  // Backup music
   server.on("/media/rick.mp3", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/media/rick.mp3", "audio/mp3");
   });
